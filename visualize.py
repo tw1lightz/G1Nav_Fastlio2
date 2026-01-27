d4b4fdfd21f9fd30f05aac0edf95b0a68b92bbb9
Zun Luo <202300104036@stumail.sztu.edu.cn>
2026-01-27 17:14:14 +0800

    将建图的rviz可视化关掉

--- visualize.py
+++ visualize.py
@@ -1,4 +1,4 @@
 import open3d as o3d
-pcd = o3d.io.read_point_cloud("D:\\G1_dev\\HongTu\\G1Nav2D\\src\\fastlio2\\PCD\\map.pcd")
+pcd = o3d.io.read_point_cloud("D:\G1_dev\HongTu\G1Nav2D\src\fastlio2\PCD\ground_map.pcd")
 print(pcd)  # 看看点数、有没有颜色
 o3d.visualization.draw_geometries([pcd])
